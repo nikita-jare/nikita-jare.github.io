908a1d0d32adad3c6713644bc302187911acaf0c
nikita-jare <nikitajare2021@gmail.com>
2024-10-17 22:56:37 -0400

    Modified vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/nikita-jare.github.io/",
+  base: "./",
   plugins: [react()],
   css: {
     postcss: {
